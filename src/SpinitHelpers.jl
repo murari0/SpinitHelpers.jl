2c068dcbcd72bf9ce5f0dcd5c947b54895b75fbd
murari0 <eaterofglass@gmail.com>
2025-12-04 12:04:44 +0000

    Add pseudo-2D export function

--- src/SpinitHelpers.jl
+++ src/SpinitHelpers.jl
@@ -1,6 +1,6 @@
 module SpinitHelpers
 
-export export1D
+export export1D, exportp2D
 include("spinit_to_ssNake.jl")
 
 end
--- src/spinit_to_ssNake.jl
+++ src/spinit_to_ssNake.jl
@@ -2,8 +2,8 @@ using JSON
 using EzXML
 using TimeZones
 
-function export1D(path, infile="data.dat", inheader="header.xml", outfile="data.json")
-    spinit_header = readxml(joinpath(path,inheader));
+function export1D(inpath; infile="data.dat", inheader="header.xml", outpath=inpath, outfile="data.json")
+    spinit_header = readxml(joinpath(inpath,inheader));
     spinit_header_needed = Dict("TRANSMIT_FREQ_1"=>"", "SPECTRAL_WIDTH"=>"", "STATE"=>"", "Nb_point"=>"", "NUMBER_OF_AVERAGES"=>"", "SEQUENCE_NAME"=>"", "RECEIVER_GAIN"=>"", "D1"=>"", "ACQUISITION_DATE"=>"");
 
     params = root(spinit_header).firstelement;
@@ -17,7 +17,7 @@ function export1D(path, infile="data.dat", inheader="header.xml", outfile="data.
         end
     end
     t = TimeZones.zdt2unix(Integer,parse(ZonedDateTime,spinit_header_needed["ACQUISITION_DATE"]));
-    metaData = Dict("# Scans" => spinit_header_needed["NUMBER_OF_AVERAGES"], "Acquisition Time [s]" => "-", "Experiment Name" => spinit_header_needed["SEQUENCE_NAME"], "Receiver Gain" => spinit_header_needed["RECEIVER_GAIN"]*" dB", "Recycle Delay [s]" => spinit_header_needed["D1"], "Sample" => "-", "Offset [Hz]" => "0.0", "Time Completed" => t, "Original dataset" => path, "raw_Time_Completed" => t, "Alternate reference" => "[]");
+    metaData = Dict("# Scans" => spinit_header_needed["NUMBER_OF_AVERAGES"], "Acquisition Time [s]" => "-", "Experiment Name" => spinit_header_needed["SEQUENCE_NAME"], "Receiver Gain" => spinit_header_needed["RECEIVER_GAIN"]*" dB", "Recycle Delay [s]" => spinit_header_needed["D1"], "Sample" => "-", "Offset [Hz]" => "0.0", "Time Completed" => t, "Original dataset" => inpath, "raw_Time_Completed" => t, "Alternate reference" => "[]");
 
     N = parse(Int32,spinit_header_needed["Nb_point"]);
     sw = parse(Float64,spinit_header_needed["SPECTRAL_WIDTH"]);
@@ -29,8 +29,8 @@ function export1D(path, infile="data.dat", inheader="header.xml", outfile="data.
         spec = 1.0;
     end
     data = Vector{Float32}(undef,2*N);
-    @assert sizeof(data) == filesize(joinpath(path,infile)) "Inconsistent data file size"
-    open(joinpath(path,infile), "r") do io
+    @assert sizeof(data) == filesize(joinpath(inpath,infile)) "Inconsistent data file size"
+    open(joinpath(inpath,infile), "r") do io
         read!(io,data)
     end
     data .= ntoh.(data);            # Spinit data is in Big-Endian format
@@ -38,5 +38,54 @@ function export1D(path, infile="data.dat", inheader="header.xml", outfile="data.
     dataImag = [data[2:2:end]];
 
     spinit_data_to_ssNake = Dict("dataReal" => dataReal, "dataImag" => dataImag, "hyper" => [0], "freq" => [parse(Float64,spinit_header_needed["TRANSMIT_FREQ_1"])], "sw" => [sw], "spec" => [spec], "wholeEcho" => [0.0], "ref" => [parse(Float64,spinit_header_needed["TRANSMIT_FREQ_1"])], "history" => ["RS2D Spinit data loaded from "*pwd()], "metaData" => metaData, "dFilter" => 0.0, "xaxArray" => xaxArray);
-    JSON.json(joinpath(path,outfile),spinit_data_to_ssNake)
+    JSON.json(joinpath(outpath,outfile),spinit_data_to_ssNake)
+end
+
+## exportND: Nb_ND is number of points in Nth dimension; DATA_REPRESENTATION has 4 <value> fields and Nth field indicates whether Nth dimension has REAL or COMPLEX points (1 COMPLEX point = 2 float values)
+
+function exportp2D(inpath; infile="data.dat", inheader="header.xml", outpath=inpath, outfile="data.json", x2D::Vector{T}=Float64[]) where T <: Real
+    spinit_header = readxml(joinpath(inpath,inheader));
+    spinit_header_needed = Dict("TRANSMIT_FREQ_1"=>"", "TRANSMIT_FREQ_2"=>"", "SPECTRAL_WIDTH"=>"", "SPECTRAL_WIDTH_2D"=>"", "STATE"=>"", "Nb_point"=>"", "Nb_2d"=>"", "NUMBER_OF_AVERAGES"=>"", "SEQUENCE_NAME"=>"", "RECEIVER_GAIN"=>"", "D1"=>"", "ACQUISITION_DATE"=>"");
+
+    params = root(spinit_header).firstelement;
+    for p in eachelement(params)
+        if p.firstelement.content == "DATA_REPRESENTATION"
+            @assert nodecontent.(findall("value/value",p))[2] == "REAL" "Full (complex) 2D files not supported"
+            break
+        end
+    end
+
+    for h in keys(spinit_header_needed)
+        for p in eachelement(params)
+            if p.firstelement.content == h
+                spinit_header_needed[h] = findfirst("value/value",p).content;
+                break
+            end
+        end
+    end
+    t = TimeZones.zdt2unix(Integer,parse(ZonedDateTime,spinit_header_needed["ACQUISITION_DATE"]));
+    metaData = Dict("# Scans" => spinit_header_needed["NUMBER_OF_AVERAGES"], "Acquisition Time [s]" => "-", "Experiment Name" => spinit_header_needed["SEQUENCE_NAME"], "Receiver Gain" => spinit_header_needed["RECEIVER_GAIN"]*" dB", "Recycle Delay [s]" => spinit_header_needed["D1"], "Sample" => "-", "Offset [Hz]" => "0.0", "Time Completed" => t, "Original dataset" => inpath, "raw_Time_Completed" => t, "Alternate reference" => "[]");
+
+    N1 = parse(Int32,spinit_header_needed["Nb_point"]);
+    N2 = parse(Int32,spinit_header_needed["Nb_2d"]);
+    sw = parse(Float64,spinit_header_needed["SPECTRAL_WIDTH"]);
+    sw2 = parse(Float64,spinit_header_needed["SPECTRAL_WIDTH_2D"]);
+    # Ignore STATE for now - bit of a mess to import half-processed data
+    if length(x2D) == 0 || length(x2D) != N2
+        @warn "No X-axis array matching second dimension provided. Using defaults"
+        xaxArray = [collect(range(start=0.0,length=N2,step=1.0)), collect(range(start=0.0,length=N1,step=1.0/sw))];
+    else
+        xaxArray = [x2D, collect(range(start=-sw/2.0,length=N,stop=sw/2.0))];
+    end
+    data = Array{Float32,2}(undef,2*N1,N2);
+    @assert sizeof(data) == filesize(joinpath(inpath,infile)) "Inconsistent data file size"
+    open(joinpath(inpath,infile), "r") do io
+        read!(io,data)
+    end
+    data .= ntoh.(data);            # Spinit data is in Big-Endian format
+    dataReal = [[data[1:2:end,i] for i in 1:N2]];
+    dataImag = [[data[2:2:end,i] for i in 1:N2]];
+
+    spinit_data_to_ssNake = Dict("dataReal" => dataReal, "dataImag" => dataImag, "hyper" => [0], "freq" => [parse(Float64,spinit_header_needed["TRANSMIT_FREQ_1"]), parse(Float64,spinit_header_needed["TRANSMIT_FREQ_2"])], "sw" => [sw, sw2], "spec" => [0.0, 0.0], "wholeEcho" => [0.0, 0.0], "ref" => [parse(Float64,spinit_header_needed["TRANSMIT_FREQ_1"]), parse(Float64,spinit_header_needed["TRANSMIT_FREQ_1"])], "history" => ["RS2D Spinit data loaded from "*pwd()], "metaData" => metaData, "dFilter" => 0.0, "xaxArray" => xaxArray);
+    JSON.json(joinpath(outpath,outfile),spinit_data_to_ssNake)
 end
